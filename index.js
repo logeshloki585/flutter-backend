3beddea2e6b8a42726859624c34c9970d170ab8e
logeshloki585 <Logeshtv21@gmail.com>
2024-09-15 17:47:18 +0530

    23456

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ mongoose.connect(process.env.MONGO_URI, {
 // Routes
 app.use('/api/auth', authRoutes);
 
+app.get('/',(req,res)=>{
+  res.send('hello');
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
